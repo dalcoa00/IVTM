d0d8fde3fcd95fd8220bb7e82039bb0a38465b83
dalcoa00 <dalcoa00@estudiantes.unileon.es>
2025-05-17 12:51:52 +0200

    Práctica 4 completa y con una ejecución "2024"

--- src/ivtm/GeneraRecibo.java
+++ src/ivtm/GeneraRecibo.java
@@ -2,9 +2,7 @@ package ivtm;
 
 public class GeneraRecibo {
     /*
-    * Una vez calculado el importe que debe pagar el vehículo en ImporteVehiculo se llama a esta clase
-    *
-    * Aquí se generan Recibos.xml y los recibos en formato PDF
+    *Clase para generar los recibos en formato PDF
     *
     * Se puede generar el Resumen PDF aquí también, pero como es mucha repetición de código igual
     * hacer en otra clase para que no nos líe el día de la modificación*/
